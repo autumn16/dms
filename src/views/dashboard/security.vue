69f808c57821fb3ba59d18bbb4ab6cf6231623ad
Minhtri <46084113+mitapari@users.noreply.github.com>
2020-08-06 01:42:54 +0700

    minor fix
    minor bug but great impact

--- src/views/dashboard/security.vue
+++ src/views/dashboard/security.vue
@@ -151,7 +151,7 @@ export default {
           icon: "mdi-clover",
           link: "../dashboard/record",
         },
-        { title: "Logout", icon: "mdi-logout-variant", link: "../signin" },
+        { title: "Logout", icon: "mdi-logout-variant", link: "../sign-in" },
       ],
     };
   },
